402671f6c8d3bda64e35fe1c66c7ab701512b315
Sean Barker <sean.barker096@gmail.com>
2022-10-17 00:05:32 +0100

    start working on uploading a file

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,30 +1,53 @@
-import React, { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-import axios from "../axios";
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function TestApiComponent() {
   const [movies, setMovies] = useState("test");
 
-  useEffect(() => {
-  axios.get("api/fileservice/0.1/test/").then((response) => {
-      console.log(response);
-      setMovies(JSON.stringify(response));
-    });
-  }, []);
+  // useEffect(() => {
+  // axios.get("api/fileservice/0.1/test/").then((response) => {
+  //     console.log(response);
+  //     setMovies(JSON.stringify(response));
+  //   });
+  // }, []);
 
   async function handleButtonClick(){
-    const body = {
-      uuid: '12345678',
-      mime_type: 'application/octet-stream'
+
+    await ImagePicker.requestMediaLibraryPermissionsAsync(false)
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: false,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    
+    console.log(result)
+
+    if (!result.cancelled) {
+      console.log(result)
+
+      console.log(result);
+
+      const response = await FileSystem.uploadAsync(`http://127.0.0.1:5000/api/fileservice/0.1/upload/1234`, result.uri, {
+      fieldName: 'bytes',
+      httpMethod: 'PATCH',
+      uploadType: FileSystem.FileSystemUploadType.BINARY_CONTENT,
+    });
+  
+    console.log("file upload response", response)
     }
-    const response = await axios.post("/api/fileservice/0.1/upload/", body, {headers: {"Accept": "application/json"}})
+
+ 
     
   }
 
   return (
     <View style={styles.container}>
       <Text>{movies}</Text>
-      <Button title="test" onPress={handleButtonClick} ></Button>
+      <Button title="Upload file" onPress={handleButtonClick} ></Button>
     </View>
   );
 }
