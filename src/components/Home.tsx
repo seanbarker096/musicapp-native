a432f4ffe810403fa73bc1240f4c84a8b89a32ec
Sean Barker <sean.barker096@gmail.com>
2022-10-28 00:48:13 +0100

    testing file uploads and downloads. Messy stuff

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,9 +5,17 @@ import { PermissionStatus } from 'expo-media-library';
 import { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+const EXT_TO_MIME_TYPE_MAP: {[index: string]: string}
+= {
+  ".jpeg": "image/jpeg",
+  ".png": "image/png"
+}
 export default function TestApiComponent() {
   const [movies, setMovies] = useState('original state');
   const [fileUri, setFileUri] = useState('')
+  const [ext, setExt] = useState("")
+
+  const uuid = '12345'
 
   useEffect(() => {
     // TODO: Need to ensure the ENV env var is passed into expo and therefore webpack build so axios works properly
@@ -24,15 +32,27 @@ export default function TestApiComponent() {
       quality: 1,
     });
 
+    let mimeType = ""
+    if(!result.cancelled){
+      console.log(result.uri)
+      const regexArray = result.uri.match(/\.[0-9a-z]+$/i)
+      const extension = regexArray ? regexArray[0] : undefined
+      setExt(extension ?? "")
+      console.log(extension)
+       mimeType = extension ? EXT_TO_MIME_TYPE_MAP[extension]: ""
+    }
+    
+
 
     if (!result.cancelled) {
+      console.log(mimeType)
 
       const response = await FileSystem.uploadAsync(`http://192.168.1.217:5000/api/fileservice/0.1/files/upload_file`, result.uri, {
       httpMethod: 'POST',
       fieldName: 'file',
       mimeType: 'multipart/form-data',
       uploadType: FileSystem.FileSystemUploadType.MULTIPART,
-      parameters: {'uuid': '12345'}
+      parameters: {'uuid': uuid, 'mime_type': mimeType} 
     });
   
     console.log("file upload response", response)
@@ -42,8 +62,8 @@ export default function TestApiComponent() {
   async function handleDownloadButtonClick(){
     try {
     const response =  await FileSystem.downloadAsync(
-      `http://192.168.1.217:5000/api/fileservice/0.1/files/12345`, 
-      FileSystem.documentDirectory + 'my_app_image.png')
+      `http://192.168.1.217:5000/api/fileservice/0.1/files/${uuid}`, 
+      FileSystem.documentDirectory + `test.jpg`)
      
         console.log(response)
         setFileUri(response.uri)
