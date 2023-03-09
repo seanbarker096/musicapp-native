5d32abf37f3c796d9a1661fb272ac1772d520856
Sean Barker <sean.barker096@gmail.com>
2023-03-09 00:48:22 +0000

    commit unsaved changes

--- src/store/profile-posts/profile-posts.query-keys.ts
+++ src/store/profile-posts/profile-posts.query-keys.ts
@@ -1,7 +1,7 @@
 import { ProfileType } from './profile-posts.types';
 
-export const profilePostKeys = {
+export const profilePostsKeys = {
   all: ['profilePosts'] as const,
   profilePostsByProfile: (profileId: number, profileType: ProfileType) =>
-    [...profilePostKeys.all, profileId, profileType] as const,
+    [...profilePostsKeys.all, profileId, profileType] as const,
 };
